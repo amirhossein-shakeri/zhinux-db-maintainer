9be90b0d56d5701608a1deb534f13cc46170058c
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2026-04-01 10:30:11 +0330

    feat: main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/amirhossein-shakeri/zhinux-platform/config"
+	"github.com/amirhossein-shakeri/zhinux-platform/logging"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// Config
+	cfg, err := config.LoadBaseFromEnv()
+	if err != nil {
+		log.Fatalf("Failed to load the config: %v", err)
+	}
+
+	logger, err := logging.NewLogger(logging.LoggerOptions{
+		Level:       cfg.LogLevel,
+		Service:     cfg.ServiceName,
+		Development: cfg.Environment == "development",
+	})
 }
